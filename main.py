1db0ad14676949711d39ec030427bcea2ae96215
Taro Morita <morita0079@gmail.com>
2022-12-19 19:14:46 +0900

    fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import re
+import requests, json
 
 out = subprocess.check_output(['sudo', 'python3', '-m', 'mh_z19']).decode('utf-8')
 
@@ -8,3 +9,11 @@ re_out = re.match(r'{\"co2\": (?P<conc>\d+)}',out)
 CO2_conce = int(re_out.group('conc'))
 print(CO2_conce)
 
+WEB_HOOK_URL = "https://hooks.slack.com/services/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"
+
+requests.post(WEB_HOOK_URL, data = json.dumps({
+    'text': u'CO2の濃度は'+ str(CO2_conce) + u' ppmです',
+    'username': 'Mr. CO2 monitor',
+    'icon_emoji': u':smile_cat:',
+    'link_names': 1,
+}))
