33c0126b2b88fee93d0b8b32b3f60a374b922fdf
Taro Morita <morita0079@gmail.com>
2023-02-07 02:20:09 +0900

    webhook urlを隠した

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import subprocess
 import re
-import requests, json
+import requests
+import json
 import database
 from sqlalchemy.orm import sessionmaker
 import co2_concentrate
-out = subprocess.check_output(['sudo', 'python3', '-m', 'mh_z19']).decode('utf-8')
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+out = subprocess.check_output(
+    ['sudo', 'python3', '-m', 'mh_z19']).decode('utf-8')
 
 
-re_out = re.match(r'{\"co2\": (?P<conc>\d+)}',out)
+re_out = re.match(r'{\"co2\": (?P<conc>\d+)}', out)
 CO2_conce = int(re_out.group('conc'))
 print(CO2_conce)
 
-WEB_HOOK_URL = "https://hooks.slack.com/services/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"
-
-requests.post(WEB_HOOK_URL, data = json.dumps({
-    'text': u'CO2の濃度は'+ str(CO2_conce) + u' ppmです',
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+WEB_HOOK_URL = os.environ.get("WEBHOOK_URL")
+requests.post(WEB_HOOK_URL, data=json.dumps({
+    'text': u'CO2の濃度は' + str(CO2_conce) + u' ppmです',
     'username': 'Mr. CO2 monitor',
     'icon_emoji': u':smile_cat:',
     'link_names': 1,
